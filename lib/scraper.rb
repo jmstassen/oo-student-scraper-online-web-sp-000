bc7c3fde2a8d4c0bed4a4004583205b81d739e59
John Stassen <john.stassen@gmail.com>
2020-09-12 22:43:25 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,27 @@
+require 'nokogiri'
 require 'open-uri'
 require 'pry'
+require_relative './student'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    doc = Nokogiri::HTML(open(index_url))
+
+    doc.css(".student-card").each do |card|
+      name = card.css("h4").text
+      location = card.css(".student-location").text
+      profile_url = card.css("a").attribute("href").text
+      student_hash = {:name=>name, :location=>location, :profile_url=>profile_url}
+      student = Student.new(student_hash)
+    end
+    Student.all
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
 
+Scraper.scrape_index_page("https://learn-co-curriculum.github.io/student-scraper-test-page/index.html")
