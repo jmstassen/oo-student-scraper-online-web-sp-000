c6ecb5b8317bd1ae307d7db6ecd0c428aaffadb2
John Stassen <john.stassen@gmail.com>
2020-09-12 23:46:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,8 +12,7 @@ class Student
   def self.create_from_collection(students_array)
     students_array.each do |student|
       Student.new(student)
-    endlearn
-
+    end
   end
 
   def add_student_attributes(attributes_hash)
