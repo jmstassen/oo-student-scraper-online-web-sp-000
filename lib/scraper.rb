2be4338b88c2aedccb8064ef84f2be74eae58637
John Stassen <john.stassen@gmail.com>
2020-09-12 23:02:25 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -20,7 +20,7 @@ class Scraper
 
   def self.scrape_profile_page(profile_url)
     doc = Nokogiri::HTML(open(profile_url))
-
+    binding.pry
     profile_quote = doc.css(".profile-quote").text
     bio = doc.css(".description-holder p").text
     twitter = doc.css(".social-icon-container a")[0].attribute("href").value
